b09ccc85ddfe0b5b7cf2361f8c87f533c8b7c274
Ryuzaki <ufrshubham@gmail.com>
2022-04-06 23:32:38 +0530

    Add basic game loop

---
+++ include/Board.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <SFML/Graphics/Drawable.hpp>
+#include <array>
+
+enum class WinStatus
+{
+    NoWin,
+    RedWin,
+    BlueWin
+};
+
+class Board : public sf::Drawable
+{
+public:
+    Board(int cellSize);
+    ~Board() = default;
+
+    enum CellType
+    {
+        Red = -1,
+        Empty = 0,
+        Blue = 1
+    };
+
+    bool PlaceInColumn(std::size_t col, const CellType &cellType);
+    WinStatus WinCheck() const;
+    bool IsGameOver() const;
+
+    sf::Vector2i GetSize() const;
+    int GetCellSide() const;
+
+#ifdef DEBUG
+    void Print() const;
+#endif
+
+private:
+    Board::CellType GetCellType(std::size_t row, std::size_t col) const;
+    bool SetCellType(std::size_t row, std::size_t col, const CellType &cellType);
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override final;
+
+private:
+    const int CELL_SIDE;
+    static const int NUMBER_OF_ROWS = 6;
+    static const int NUMBER_OF_COLUMNS = 7;
+
+    std::array<Board::CellType, NUMBER_OF_ROWS * NUMBER_OF_COLUMNS> m_state;
+};
---
+++ src/Board.cpp
@@ -0,0 +1,171 @@
+#include "Board.hpp"
+#include <cassert>
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+
+#ifdef DEBUG
+#include <iostream>
+#endif
+
+Board::Board(int cellSize) : CELL_SIDE(cellSize)
+{
+    m_state.fill(Board::CellType::Empty);
+}
+
+bool Board::PlaceInColumn(std::size_t col, const CellType &cellType)
+{
+    bool placed = false;
+
+    for (int row = NUMBER_OF_ROWS - 1; row >= 0; --row)
+    {
+        if (GetCellType(row, col) == CellType::Empty)
+        {
+            placed = SetCellType(row, col, cellType);
+            break;
+        }
+    }
+
+    return placed;
+}
+
+WinStatus Board::WinCheck() const
+{
+    WinStatus winStatus = WinStatus::NoWin;
+
+    for (std::size_t row = 0; row < NUMBER_OF_ROWS; ++row)
+    {
+        for (std::size_t col = 0; col < NUMBER_OF_COLUMNS; ++col)
+        {
+            auto currentCellType = GetCellType(row, col);
+            if (currentCellType != CellType::Empty)
+            {
+                if (col < NUMBER_OF_COLUMNS - 4)
+                {
+                    auto sum = std::abs(currentCellType + GetCellType(row, col + 1) + GetCellType(row, col + 2) + GetCellType(row, col + 3));
+                    if (sum == 4)
+                    {
+                        winStatus = currentCellType == CellType::Red ? WinStatus::RedWin : WinStatus::BlueWin;
+#ifdef DEBUG
+                        std::cout << (winStatus == WinStatus::RedWin ? "Red " : "Blue ")
+                                  << "Won" << std::endl;
+#endif
+                    }
+                }
+            }
+        }
+    }
+
+    return winStatus;
+}
+
+bool Board::IsGameOver() const
+{
+    return std::all_of(m_state.begin(), m_state.end(), [](const auto &cellType)
+                       { return cellType; });
+}
+
+sf::Vector2i Board::GetSize() const
+{
+    return {Board::CELL_SIDE * Board::NUMBER_OF_COLUMNS, Board::CELL_SIDE * Board::NUMBER_OF_ROWS};
+}
+
+int Board::GetCellSide() const
+{
+    return CELL_SIDE;
+}
+
+#ifdef DEBUG
+void Board::Print() const
+{
+    for (std::size_t row = 0; row < NUMBER_OF_ROWS; ++row)
+    {
+        for (std::size_t col = 0; col < NUMBER_OF_COLUMNS; ++col)
+        {
+            std::cout << GetCellType(row, col) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+#endif
+
+Board::CellType Board::GetCellType(std::size_t row, std::size_t col) const
+{
+    bool isInputValid = (row >= 0 && row < NUMBER_OF_ROWS && col >= 0 && col < NUMBER_OF_COLUMNS);
+    assert(isInputValid);
+
+    CellType cellType = CellType::Empty;
+    if (isInputValid)
+    {
+        cellType = m_state[row * NUMBER_OF_COLUMNS + col];
+    }
+
+    return cellType;
+}
+
+bool Board::SetCellType(std::size_t row, std::size_t col, const CellType &cellType)
+{
+    bool isInputValid = (row >= 0 && row < NUMBER_OF_ROWS && col >= 0 && col < NUMBER_OF_COLUMNS);
+    assert(isInputValid);
+
+    bool placed = false;
+
+    if (isInputValid && (GetCellType(row, col) == CellType::Empty))
+    {
+        m_state[row * NUMBER_OF_COLUMNS + col] = cellType;
+        placed = true;
+    }
+
+    return placed;
+}
+
+void Board::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+
+    for (std::size_t row = 0; row < NUMBER_OF_ROWS; ++row)
+    {
+        for (std::size_t col = 0; col < NUMBER_OF_COLUMNS; ++col)
+        {
+            sf::RectangleShape rect({(float)CELL_SIDE, (float)CELL_SIDE});
+            rect.setPosition({(float)col * CELL_SIDE, (float)row * CELL_SIDE});
+            rect.setFillColor(sf::Color::Black);
+            rect.setOutlineThickness(2);
+            rect.setOutlineColor(sf::Color::White);
+
+            target.draw(rect);
+
+            switch (GetCellType(row, col))
+            {
+            case CellType::Empty:
+            {
+                break;
+            }
+            case CellType::Red:
+            {
+                sf::CircleShape circle(40);
+                circle.setFillColor(sf::Color::Red);
+                circle.setOutlineThickness(2);
+                circle.setOutlineColor(sf::Color::White);
+                auto localBoundingBox = circle.getLocalBounds();
+                circle.setOrigin(localBoundingBox.width / 2, localBoundingBox.height / 2);
+                circle.setPosition(sf::Vector2f{(float)col * CELL_SIDE, (float)row * CELL_SIDE} + sf::Vector2f{CELL_SIDE / 2.f, CELL_SIDE / 2.f});
+                target.draw(circle);
+                break;
+            }
+            case CellType::Blue:
+            {
+                sf::CircleShape circle(40);
+                circle.setFillColor(sf::Color::Blue);
+                circle.setOutlineThickness(2);
+                circle.setOutlineColor(sf::Color::White);
+                auto localBoundingBox = circle.getLocalBounds();
+                circle.setOrigin(localBoundingBox.width / 2, localBoundingBox.height / 2);
+                circle.setPosition(sf::Vector2f{(float)col * CELL_SIDE, (float)row * CELL_SIDE} + sf::Vector2f{CELL_SIDE / 2.f, CELL_SIDE / 2.f});
+                target.draw(circle);
+                break;
+            }
+            }
+        }
+    }
+}
